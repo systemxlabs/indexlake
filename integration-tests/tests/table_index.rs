7b6d0104c624b2d51ad9206eced1faed57b0dde6
张林伟 <lewiszlw520@gmail.com>
2025-09-10 17:59:58 +0800

    Move table index tests into new file

---
+++ integration-tests/tests/table_index.rs
@@ -0,0 +1,137 @@
+use indexlake::Client;
+use indexlake::catalog::Catalog;
+use indexlake::index::IndexKind;
+use indexlake::storage::{DataFileFormat, Storage};
+use indexlake::table::IndexCreation;
+use indexlake_index_rstar::{RStarIndexKind, RStarIndexParams, WkbDialect};
+use indexlake_integration_tests::data::prepare_simple_geom_table;
+use indexlake_integration_tests::{
+    catalog_postgres, catalog_sqlite, init_env_logger, storage_fs, storage_s3,
+};
+use std::sync::Arc;
+
+#[rstest::rstest]
+#[case(async { catalog_sqlite() }, async { storage_fs() })]
+#[case(async { catalog_postgres().await }, async { storage_s3().await })]
+#[tokio::test(flavor = "multi_thread")]
+async fn duplicated_index_name(
+    #[future(awt)]
+    #[case]
+    catalog: Arc<dyn Catalog>,
+    #[future(awt)]
+    #[case]
+    storage: Arc<Storage>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    init_env_logger();
+
+    let mut client = Client::new(catalog, storage);
+    client.register_index_kind(Arc::new(RStarIndexKind));
+
+    let table = prepare_simple_geom_table(&client, DataFileFormat::ParquetV2).await?;
+    let namespace_name = table.namespace_name.clone();
+    let table_name = table.table_name.clone();
+
+    let mut index_creation = IndexCreation {
+        name: uuid::Uuid::new_v4().to_string(),
+        kind: RStarIndexKind.kind().to_string(),
+        key_columns: vec!["geom".to_string()],
+        params: Arc::new(RStarIndexParams {
+            wkb_dialect: WkbDialect::Wkb,
+        }),
+        if_not_exists: false,
+    };
+
+    table.create_index(index_creation.clone()).await?;
+
+    let table = client.load_table(&namespace_name, &table_name).await?;
+    let result = table.create_index(index_creation.clone()).await;
+    assert!(result.is_err());
+
+    index_creation.if_not_exists = true;
+    let table = client.load_table(&namespace_name, &table_name).await?;
+    table.create_index(index_creation).await?;
+
+    Ok(())
+}
+
+#[rstest::rstest]
+#[case(async { catalog_sqlite() }, async { storage_fs() })]
+#[case(async { catalog_postgres().await }, async { storage_s3().await })]
+#[tokio::test(flavor = "multi_thread")]
+async fn unsupported_index_kind(
+    #[future(awt)]
+    #[case]
+    catalog: Arc<dyn Catalog>,
+    #[future(awt)]
+    #[case]
+    storage: Arc<Storage>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    init_env_logger();
+
+    let mut client = Client::new(catalog, storage);
+    client.register_index_kind(Arc::new(RStarIndexKind));
+
+    let table = prepare_simple_geom_table(&client, DataFileFormat::ParquetV2).await?;
+
+    let index_creation = IndexCreation {
+        name: uuid::Uuid::new_v4().to_string(),
+        kind: "unsupported_index_kind".to_string(),
+        key_columns: vec!["geom".to_string()],
+        params: Arc::new(RStarIndexParams {
+            wkb_dialect: WkbDialect::Wkb,
+        }),
+        if_not_exists: false,
+    };
+
+    let result = table.create_index(index_creation).await;
+    assert!(result.is_err());
+
+    Ok(())
+}
+
+#[rstest::rstest]
+#[case(async { catalog_sqlite() }, async { storage_fs() })]
+#[case(async { catalog_postgres().await }, async { storage_s3().await })]
+#[tokio::test(flavor = "multi_thread")]
+async fn drop_index(
+    #[future(awt)]
+    #[case]
+    catalog: Arc<dyn Catalog>,
+    #[future(awt)]
+    #[case]
+    storage: Arc<Storage>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    init_env_logger();
+
+    let mut client = Client::new(catalog, storage);
+    client.register_index_kind(Arc::new(RStarIndexKind));
+
+    let table = prepare_simple_geom_table(&client, DataFileFormat::ParquetV2).await?;
+    let namespace_name = table.namespace_name.clone();
+    let table_name = table.table_name.clone();
+
+    let index_name = uuid::Uuid::new_v4().to_string();
+    let index_creation = IndexCreation {
+        name: index_name.to_string(),
+        kind: RStarIndexKind.kind().to_string(),
+        key_columns: vec!["geom".to_string()],
+        params: Arc::new(RStarIndexParams {
+            wkb_dialect: WkbDialect::Wkb,
+        }),
+        if_not_exists: false,
+    };
+
+    table.create_index(index_creation.clone()).await?;
+
+    let table = client.load_table(&namespace_name, &table_name).await?;
+    table.drop_index(&index_name, false).await?;
+
+    let table = client.load_table(&namespace_name, &table_name).await?;
+    let result = table.drop_index(&index_name, false).await;
+    assert!(result.is_err());
+
+    let table = client.load_table(&namespace_name, &table_name).await?;
+    table.create_index(index_creation).await?;
+
+    Ok(())
+}
--- integration-tests/tests/table_ops.rs
+++ integration-tests/tests/table_ops.rs
@@ -3,11 +3,9 @@ use arrow::datatypes::*;
 use futures::TryStreamExt;
 use indexlake::Client;
 use indexlake::catalog::{Catalog, INTERNAL_ROW_ID_FIELD_REF};
-use indexlake::index::IndexKind;
 use indexlake::storage::{DataFileFormat, Storage};
-use indexlake::table::{IndexCreation, TableConfig, TableCreation, TableInsertion, TableScan};
-use indexlake_index_rstar::{RStarIndexKind, RStarIndexParams, WkbDialect};
-use indexlake_integration_tests::data::{prepare_simple_geom_table, prepare_simple_testing_table};
+use indexlake::table::{TableConfig, TableCreation, TableInsertion, TableScan};
+use indexlake_integration_tests::data::prepare_simple_testing_table;
 use indexlake_integration_tests::utils::{
     assert_data_file_count, assert_inline_row_count, full_table_scan,
 };
@@ -551,132 +549,6 @@ async fn drop_table(
 | 2  | Bob   |
 +----+-------+"#,
     );
-
-    Ok(())
-}
-
-#[rstest::rstest]
-#[case(async { catalog_sqlite() }, async { storage_fs() })]
-#[case(async { catalog_postgres().await }, async { storage_s3().await })]
-#[tokio::test(flavor = "multi_thread")]
-async fn duplicated_index_name(
-    #[future(awt)]
-    #[case]
-    catalog: Arc<dyn Catalog>,
-    #[future(awt)]
-    #[case]
-    storage: Arc<Storage>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    init_env_logger();
-
-    let mut client = Client::new(catalog, storage);
-    client.register_index_kind(Arc::new(RStarIndexKind));
-
-    let table = prepare_simple_geom_table(&client, DataFileFormat::ParquetV2).await?;
-    let namespace_name = table.namespace_name.clone();
-    let table_name = table.table_name.clone();
-
-    let mut index_creation = IndexCreation {
-        name: uuid::Uuid::new_v4().to_string(),
-        kind: RStarIndexKind.kind().to_string(),
-        key_columns: vec!["geom".to_string()],
-        params: Arc::new(RStarIndexParams {
-            wkb_dialect: WkbDialect::Wkb,
-        }),
-        if_not_exists: false,
-    };
-
-    table.create_index(index_creation.clone()).await?;
-
-    let table = client.load_table(&namespace_name, &table_name).await?;
-    let result = table.create_index(index_creation.clone()).await;
-    assert!(result.is_err());
-
-    index_creation.if_not_exists = true;
-    let table = client.load_table(&namespace_name, &table_name).await?;
-    table.create_index(index_creation).await?;
-
-    Ok(())
-}
-
-#[rstest::rstest]
-#[case(async { catalog_sqlite() }, async { storage_fs() })]
-#[case(async { catalog_postgres().await }, async { storage_s3().await })]
-#[tokio::test(flavor = "multi_thread")]
-async fn unsupported_index_kind(
-    #[future(awt)]
-    #[case]
-    catalog: Arc<dyn Catalog>,
-    #[future(awt)]
-    #[case]
-    storage: Arc<Storage>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    init_env_logger();
-
-    let mut client = Client::new(catalog, storage);
-    client.register_index_kind(Arc::new(RStarIndexKind));
-
-    let table = prepare_simple_geom_table(&client, DataFileFormat::ParquetV2).await?;
-
-    let index_creation = IndexCreation {
-        name: uuid::Uuid::new_v4().to_string(),
-        kind: "unsupported_index_kind".to_string(),
-        key_columns: vec!["geom".to_string()],
-        params: Arc::new(RStarIndexParams {
-            wkb_dialect: WkbDialect::Wkb,
-        }),
-        if_not_exists: false,
-    };
-
-    let result = table.create_index(index_creation).await;
-    assert!(result.is_err());
-
-    Ok(())
-}
-
-#[rstest::rstest]
-#[case(async { catalog_sqlite() }, async { storage_fs() })]
-#[case(async { catalog_postgres().await }, async { storage_s3().await })]
-#[tokio::test(flavor = "multi_thread")]
-async fn drop_index(
-    #[future(awt)]
-    #[case]
-    catalog: Arc<dyn Catalog>,
-    #[future(awt)]
-    #[case]
-    storage: Arc<Storage>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    init_env_logger();
-
-    let mut client = Client::new(catalog, storage);
-    client.register_index_kind(Arc::new(RStarIndexKind));
-
-    let table = prepare_simple_geom_table(&client, DataFileFormat::ParquetV2).await?;
-    let namespace_name = table.namespace_name.clone();
-    let table_name = table.table_name.clone();
-
-    let index_name = uuid::Uuid::new_v4().to_string();
-    let index_creation = IndexCreation {
-        name: index_name.to_string(),
-        kind: RStarIndexKind.kind().to_string(),
-        key_columns: vec!["geom".to_string()],
-        params: Arc::new(RStarIndexParams {
-            wkb_dialect: WkbDialect::Wkb,
-        }),
-        if_not_exists: false,
-    };
-
-    table.create_index(index_creation.clone()).await?;
-
-    let table = client.load_table(&namespace_name, &table_name).await?;
-    table.drop_index(&index_name, false).await?;
-
-    let table = client.load_table(&namespace_name, &table_name).await?;
-    let result = table.drop_index(&index_name, false).await;
-    assert!(result.is_err());
-
-    let table = client.load_table(&namespace_name, &table_name).await?;
-    table.create_index(index_creation).await?;
 
     Ok(())
 }
