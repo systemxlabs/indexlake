e05d1dd3e30ed7f21cea9ef4c630a317c25d6868
张林伟 <lewiszlw520@gmail.com>
2026-02-06 15:25:00 +0800

    fix fmt

--- indexlake/src/table/dump.rs
+++ indexlake/src/table/dump.rs
@@ -32,7 +32,7 @@ pub(crate) async fn try_run_dump_task(table: &Table) -> ILResult<()> {
             if catalog_helper.task_exists(&task_id).await? {
                 return Ok(false);
             }
-            
+
             let inline_row_count = catalog_helper.count_inline_rows(&table_id, &[]).await?;
             if inline_row_count < table_config.inline_row_count_limit as i64 {
                 return Ok(false);
