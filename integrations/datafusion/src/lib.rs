0eabbe546bf073168820ef20bc6212620732f38d
Linwei Zhang <lewiszlw520@gmail.com>
2026-06-15 16:38:26 +0800

    refactor: extract schema_projection_equals to crate root for reuse

--- integrations/datafusion/src/lib.rs
+++ integrations/datafusion/src/lib.rs
@@ -17,3 +17,17 @@ pub use scan::*;
 pub use search::*;
 pub use table::*;
 pub use update::*;
+
+use arrow::datatypes::Schema;
+
+pub(crate) fn schema_projection_equals(left: &Schema, right: &Schema) -> bool {
+    if left.fields.len() != right.fields.len() {
+        return false;
+    }
+    for (left_field, right_field) in left.fields.iter().zip(right.fields.iter()) {
+        if left_field.name() != right_field.name() {
+            return false;
+        }
+    }
+    true
+}
